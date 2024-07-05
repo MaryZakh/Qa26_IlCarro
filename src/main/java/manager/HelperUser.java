091e8c0e59c3b226e849ee5906d2e7adf4892ac8
Maria Z <z0559882272@gmail.com>
2024-07-05 10:59:03 +0300

    login success

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,39 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLoginForm() {
+        click(By.xpath("//*[text()=' Log in ']"));
+
+    }
+
+
+    public void fillLoginForm(String email, String password) {
+        type(By.id("email"), email);
+        type(By.id("password"), password);
+    }
+
+    public void submitLogin() {
+        click(By.xpath("//*[@type='submit']"));
+    }
+
+    public void clickOkButton() {
+        click(By.xpath("//button[text()='Ok']"));
+    }
+
+    public String getMessage() {
+       // pause(1000);
+        return wd.findElement(By.cssSelector(".dialog-container>h2")).getText();
+//        WebElement element = wd.findElement(By.cssSelector(".dialog-container>h2"));
+//        String text = element.getText();
+//        return text;
+
     }
 }
