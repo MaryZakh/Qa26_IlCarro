8a978b68f9c6dec7bf975048e797791b8a5f06f9
Maria Z <z0559882272@gmail.com>
2024-07-30 19:03:29 +0300

    searchCurrentYear

--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class HelperCar extends HelperBase{
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class HelperCar extends HelperBase {
     public HelperCar(WebDriver wd) {
         super(wd);
     }
@@ -20,11 +23,11 @@ public class HelperCar extends HelperBase{
         type(By.id("make"), car.getManufacture());
         type(By.id("model"), car.getModel());
         type(By.id("year"), car.getYear());
-        select(By.id("fuel"),car.getFuel());
-        type(By.id("seats"),String.valueOf(car.getSeats()));
+        select(By.id("fuel"), car.getFuel());
+        type(By.id("seats"), String.valueOf(car.getSeats()));
         type(By.id("class"), car.getCarClass());
         type(By.id("serialNumber"), car.getCarRegNumber());
-        type(By.id("price"), car.getPrice()+"");
+        type(By.id("price"), car.getPrice() + "");
         type(By.id("about"), car.getAbout());
 
     }
@@ -40,7 +43,7 @@ public class HelperCar extends HelperBase{
     }
 
     private void typeLocation(String location) {
-        type(By.id("pickUpPlace"),location);
+        type(By.id("pickUpPlace"), location);
         click(By.cssSelector("div.pac-item"));
     }
 
@@ -51,4 +54,79 @@ public class HelperCar extends HelperBase{
     public void attachPhoto(String link) {
         wd.findElement(By.cssSelector("#photos")).sendKeys(link);
     }
+
+    public void searchCurrentMonth(String city, String dateFrom, String dateTo) {
+        typeCity(city);
+        click(By.id("dates"));
+
+        // "7/27/2024", "7/30/2024"  27  30
+        String[] from = dateFrom.split("/"); //["7"]["27"]["2024"]
+
+
+        String locatorFrom = "//div[text()=' " + from[1] + " ']";
+        click(By.xpath(locatorFrom));
+
+
+        String[] to = dateTo.split("/");
+
+        click(By.xpath("//div[text()=' " + to[1] + " ']"));
+
+    }
+
+    private void typeCity(String city) {
+        type(By.id("city"), city);
+        click(By.cssSelector("div.pac-item"));
+    }
+
+    public boolean isListOfCarsAppeared() {
+        return isElementPresent(By.cssSelector("a.car-container"));
+    }
+
+    public void searchCurrentYear(String city, String dateFrom, String dateTo) {
+        // "10/15/2024", "12/10/2024"
+
+        typeCity(city);
+        click(By.id("dates"));
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+        int year = now.getYear();
+        int month = now.getMonthValue();
+        int day = now.getDayOfMonth();
+
+        LocalDate from = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));//2024-07-30
+        LocalDate to = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("M/d/yyyy"));
+
+        System.out.println(from);
+//        LocalDate from1 = LocalDate.parse("2013:23/05",DateTimeFormatter.ofPattern("yyyy:dd/MM"));
+//        System.out.println(from1);
+
+
+        int diffMonth = from.getMonthValue() - month;
+        if (diffMonth > 0) {
+            clickNextMonthBtn(diffMonth);
+        }
+        click(By.xpath("//div[text()=' " + from.getDayOfMonth() + " ']"));
+
+
+        diffMonth = to.getMonthValue()-from.getMonthValue();
+        if (diffMonth>0){
+            clickNextMonthBtn(diffMonth);
+        }
+        ////div[text()=' " + to[1] + " ']"
+        String locator = String.format("//div[text()=' %s ']", to.getDayOfMonth());
+        click(By.xpath(locator));
+
+    }
+
+    private void clickNextMonthBtn(int diffMonth) {
+        for (int i = 0; i < diffMonth; i++) {
+            click(By.cssSelector("button[aria-label='Next month']"));
+        }
+
+
+
+    }
+
 }
---
+++ src/test/java/tests/SearchCarTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchCarTests extends TestBase {
+
+    @Test
+    public void searchCurrentMonthSuccess() {
+        app.getHelperCar().searchCurrentMonth("Tel Aviv, Israel", "7/30/2024", "7/31/2024");
+        app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+
+    }
+
+
+    @Test
+    public void searchCurrentYearSuccess() {
+        app.getHelperCar().searchCurrentYear("Rehovot", "10/15/2024", "12/10/2024");
+        app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+
+    }
+
+//    @Test
+//    public void searchAnyPeriodSuccess() {
+//        app.getHelperCar().searchAnyPeriod("Rehovot", "9/26/2024", "3/8/2025");
+//        app.getHelperCar().submit();
+//        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+//    }
+
+}
